ab59cc7c93aba3de359c8075e6ef1c5f27dea44c
ramatronics1 <shrihegde661@gmail.com>
2025-09-17 18:10:34 +0530

    tic tac toe mini project to understand useState hook in react

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,10 +1,35 @@
+import Player from "./components/Player"
+import GameBoard from "./components/GameBord"
+import { useState } from "react"
+import Log from "./components/Log"
 
 function App() {
-  
+ const [gameTurns, setGameTurns] = useState([])
+ const [activePlayer, setActivePlayer] =useState('X')
 
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+ function handleselectSquare(rowIndex , colIndex){
+    setActivePlayer((currentActivePlayer) => currentActivePlayer ==='X'? 'O': 'X');
+    setGameTurns(prevTurns => {
+      let currentPlayer = 'X';
+      console.log("condition", prevTurns.length > 0 && prevTurns[0].player === 'X')
+      if(prevTurns.length > 0 && prevTurns[0].player === 'X'){
+        currentPlayer === 'O';
+      }
+      const updateTurns = [{square:{row: rowIndex, col: colIndex}, player: currentPlayer  },...prevTurns];
+      return updateTurns
+    })
+ }
+
+  return <main>
+    <div id="game-container">
+      <ol id="players" className="highlight-player">
+       <Player initialName ="Player 1" symbol="X" isActive={activePlayer==='X'}/>
+       <Player initialName ="Player 2" symbol="O" isActive={activePlayer==='O'}/>
+      </ol>
+      <GameBoard onSelectSquare={handleselectSquare} turns = {gameTurns}/>
+    </div>
+    <Log/>
+  </main>
 }
 
 export default App
---
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBord.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const initialGameBoard = [[null, null, null],
+[null, null, null],
+[null, null, null]];
+
+export default function GameBoard({onSelectSquare, turns}) {
+    let gameBoard = initialGameBoard;
+
+    for (const turn of turns){
+        const {square, player} = turn;
+        const {row, col} = square;
+
+        gameBoard[row][col] = player;
+    }
+    // const [gameBoard, setGameBoard] = useState(initialGameBoard)
+
+    // function handleselectSquare(rowIndex, colIndex){
+    //     setGameBoard((prevGameBoard) => {
+    //         const updateBoard=[...prevGameBoard.map(innerArray => [...innerArray])];
+    //         updateBoard[rowIndex][colIndex] = activeSymbol;
+    //         return updateBoard;
+    //     });
+    //     onSelectSquare();
+    // }
+
+    return <ol id="game-board">
+        {gameBoard.map((row, rowIndex) =>(
+            <li key={rowIndex}>
+                <ol>
+                    {row.map((playerSymbol, colIndex) => (
+                        <li key={colIndex}>
+                            <button onClick={()=> onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                        </li>
+                    ))}
+                </ol>
+            </li>
+        ))}
+    </ol>
+}
